004a18c038e98b3c2e6fae74df118e32cabe7747
pranceraz <arnavyadavilli@gmail.com>
2025-06-21 23:32:20 -0700

    added comments changed landmarker loop for simplicity

--- main.py
+++ main.py
@@ -11,18 +11,16 @@ def draw_landmarks_on_image(rgb_image, detection_result):
     pose_landmarks_list = detection_result.pose_landmarks
     annotated_image = np.copy(rgb_image)
 
-    for idx in range(len(pose_landmarks_list)):
-        pose_landmarks = pose_landmarks_list[idx]
-
+    for pose_landmarks in pose_landmarks_list: #pose_landmarks_list is a list of people detected not landmarks 
         pose_landmarks_proto = landmark_pb2.NormalizedLandmarkList()
         pose_landmarks_proto.landmark.extend([
             landmark_pb2.NormalizedLandmark(x=landmark.x, y=landmark.y, z=landmark.z)
-            for landmark in pose_landmarks])
+            for landmark in pose_landmarks]) # adds normalised landmarks to each the pose_landmarks_proto list of which is an empty list of Normalised Landmarks
         solutions.drawing_utils.draw_landmarks(
             annotated_image,
             pose_landmarks_proto,
             solutions.pose.POSE_CONNECTIONS,
-            solutions.drawing_styles.get_default_pose_landmarks_style())
+            solutions.drawing_styles.get_default_pose_landmarks_style()) # I assume this is the drawing tool
     return annotated_image
 
 def main():
